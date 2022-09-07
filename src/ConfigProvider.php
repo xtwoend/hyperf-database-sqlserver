b1eebdb3d0739253c3666600872187cec0ba862c
Abdul Hafidz A <aditans88@gmail.com>
2022-09-07 09:53:44 +0700

    init

---
+++ src/ConfigProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ */
+
+namespace Hyperf\Database\Sqlsvr;
+
+use Hyperf\Database\Sqlsvr\Connectors\SqlServerConnector;
+use Hyperf\Database\Sqlsvr\Listener\RegisterConnectionListener;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+                'db.connector.sqlsrv' => SqlServerConnector::class,
+            ],
+            'listeners' => [
+                RegisterConnectionListener::class
+            ],
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
+                ],
+            ],
+        ];
+    }
+}
---
+++ src/Listener/RegisterConnectionListener.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Hyperf\Database\Sqlsvr\Listener;
+
+use Hyperf\Contract\ContainerInterface;
+use Hyperf\Database\Sqlsvr\SqlServerConnection;
+use Hyperf\Event\Contract\ListenerInterface;
+use Hyperf\Framework\Event\BootApplication;
+
+final class RegisterConnectionListener implements ListenerInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function listen(): array
+    {
+        return [
+            BootApplication::class,
+        ];
+    }
+
+    public function process(object $event): void
+    {
+        \Hyperf\Database\Connection::resolverFor('sqlsrv', function($connection, $database, $prefix, $config){
+            return new SqlServerConnection($connection, $database, $prefix, $config);
+        });
+    }
+}
